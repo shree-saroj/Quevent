de53fc2cdaf5c609f63649dddee1533ac691135f
shree-saroj <s.saroj0048@gmail.com>
2025-05-25 04:00:30 +0545

    fix: updated htaccess and added few changes

---
+++ config/paths.php
@@ -0,0 +1,6 @@
+<?php
+    define('BASE_PATH', dirname(__DIR__) . '/');
+    define('TEMPLATE_PATH', BASE_PATH . 'templates/');
+    define('ADMIN_TEMPLATE_PATH', TEMPLATE_PATH . 'admin/');
+    define('USER_TEMPLATE_PATH', TEMPLATE_PATH . 'user/');
+?>
---
+++ templates/admin/footer.php
@@ -0,0 +1,24 @@
+        <footer>
+          <div class="footer clearfix mb-0 text-muted">
+            <div class="float-start">
+              <p>2023 &copy; Mazer</p>
+            </div>
+            <div class="float-end">
+              <p>
+                Crafted with
+                <span class="text-danger"
+                  ><i class="bi bi-heart-fill icon-mid"></i
+                ></span>
+                by <a href="https://saugi.me">Saugi</a>
+              </p>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </div>
+    <script src="/assets/plugins/mazor/static/js/components/dark.js"></script>
+    <script src="/assets/plugins/mazor/extensions/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+
+    <script src="/assets/plugins/mazor/compiled/js/app.js"></script>
+  </body>
+</html>
