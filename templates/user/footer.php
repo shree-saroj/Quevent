e03dffe5652fb5725463854a1185b9f987779a42
shree-saroj <s.saroj0048@gmail.com>
2025-05-25 05:39:25 +0545

    fix: layour for user

---
+++ templates/user/footer.php
@@ -0,0 +1,28 @@
+
+        <footer>
+          <div class="container">
+            <div class="footer clearfix mb-0 text-muted">
+              <div class="float-start">
+                <p>2023 &copy; Mazer</p>
+              </div>
+              <div class="float-end">
+                <p>
+                  Crafted with
+                  <span class="text-danger"><i class="bi bi-heart"></i></span>
+                  by <a href="https://saugi.me">Saugi</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </div>
+    <script src="/assets/plugins/mazor/static/js/components/dark.js"></script>
+    <script src="/assets/plugins/mazor/static/js/pages/horizontal-layout.js"></script>
+    <script src="/assets/plugins/mazor/extensions/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+
+    <script src="/assets/plugins/mazor/compiled/js/app.js"></script>
+
+  </body>
+</html>
+
